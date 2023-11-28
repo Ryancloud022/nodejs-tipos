72e5bc4cdba994b34c78dd7d8fdcc3cc66fc773d
Ryancloud022 <ryanpedrozacloud@gmail.com>
2023-11-28 15:58:21 -0300

    1 commit

---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "senha12345!"
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Ryan";
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+// template string OU template literal 
+
